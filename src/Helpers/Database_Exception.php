f3e3e2addca4cce86d9de05b9a9417f392d253bb
Mattias Forsman <forsman1991@gmail.com>
2023-06-15 13:46:31 +0200

    Fix PHP 8 argument count error

--- src/Helpers/Database_Exception.php
+++ src/Helpers/Database_Exception.php
@@ -9,7 +9,7 @@ class Database_Exception extends \Exception {
 	protected $_args;
 
 	
-	public function __construct($message = '', $code = 0 , ?Throwable $previous, $query = null, $args = null) {
+	public function __construct($message = '', $code = 0 , ?Throwable $previous = null, $query = null, $args = null) {
 		$this->_query = $query;
 		$this->_args = $args;
 		
